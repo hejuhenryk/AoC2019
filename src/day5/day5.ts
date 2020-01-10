57b6d0004f3cc00d5aace54c48d26e92733f29fc
Marcin Sawczuk-Szymkiewicz <sawczuk.marcin@gmail.com>
2020-01-10 11:29:45 +0100

    day5 - part 2

--- src/day5/day5.ts
+++ src/day5/day5.ts
@@ -9,28 +9,12 @@ fetch('src/day5/day5_input.txt')
         while(!TEST.isFinished()) {
             TEST.nextStep()
         }
-        console.log(TEST.getOutputs())
+        console.log('part 1',TEST.getOutputs())
+        const TEST2 = intComputer(programArr, 5)
+        while(!TEST2.isFinished()) {
+            TEST2.nextStep()
+        }
+        console.log('part 2',TEST2.getOutputs())
     } )
 
 
-//     const input = [1,9,10,3,2,3,11,0,99,30,40,50]
-// console.log('test')
-// const test = intComputer(input, 1)
-// test.nextStep()
-// console.log(test.isFinished())
-
-
-// const decoder = (inputCode: number) => {
-//     const sign = inputCode >= 0 ? 1 : -1
-//     inputCode = Math.abs(inputCode)
-//     let code: number[] = inputCode.toString().split('').map(n=>+n)
-//     console.log(code)
-//     const third = code.length == 5 ? code[0] : 0
-//     const second = code.length >= 4 ? code[code.length - 4] : 0
-//     const first = code.length >= 3 ? code[code.length - 3] : 0
-//     let opcode = +((code.length >= 2 ? code[code.length - 2] : 0) + code[code.length-1].toString())*sign
-
-//     return { first, second, third, opcode }
-// }
-
-// console.log(decoder(99))
